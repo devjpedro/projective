a129dcdd75f101ccec5bc8ca10abb4ff8f6bb136
devjpedro <joaopedross.ti9@gmail.com>
2025-04-24 18:20:28 -0300

    add: get membership organization route

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,12 @@
 import 'fastify'
 
+import type { Member, Organization } from '@/generated/prisma'
+
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
+    getUserMembership(
+      slug: string,
+    ): Promise<{ organization: Organization; membership: Member }>
   }
 }
