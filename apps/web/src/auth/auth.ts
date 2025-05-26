4238f5bcdcad0eb9afccf6d85ccf29ad807bbce0
devjpedro <joaopedross.ti9@gmail.com>
2025-05-26 20:11:23 -0300

    add: obtain active user

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,4 +1,7 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export const isAuthenticated = async () => {
   const cookieStore = await cookies()
@@ -6,4 +9,20 @@ export const isAuthenticated = async () => {
   const token = cookieStore.get('token')?.value
 
   return !!token
+}
+
+export async function auth() {
+  const cookieStore = await cookies()
+
+  const token = cookieStore.get('token')?.value
+
+  if (!token) redirect('/auth/sign-in')
+
+  try {
+    const { user } = await getProfile()
+
+    return { user }
+  } catch {}
+
+  redirect('/api/auth/sign-out')
 }
