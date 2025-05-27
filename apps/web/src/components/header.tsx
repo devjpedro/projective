e303d816fa258f9f7428b970d369d5389c1941b8
devjpedro <joaopedross.ti9@gmail.com>
2025-05-27 18:59:37 -0300

    add: component header

---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+import logoIcon from '@/assets/logo-icon.svg'
+import { ProfileButton } from './profile-button'
+
+
+export function Header() {
+  return (
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image src={logoIcon} alt="" className="size-6 dark:invert" />
+      </div>
+
+      <div className="flex items-center gap-4">
+        <ProfileButton />
+      </div>
+    </div>
+  )
+}
---
+++ apps/web/src/components/profile-button.tsx
@@ -0,0 +1,51 @@
+import { ChevronDown, LogOut } from 'lucide-react'
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import Link from 'next/link'
+import { auth } from '@/auth/auth'
+
+function getInitials(name: string | null): string {
+  if (!name) return ''
+
+  const words = name.trim().split(/\s+/)
+  const initials = words.slice(0, 2).map((word) => word.charAt(0).toUpperCase())
+
+  return initials.join('')
+}
+
+export async function ProfileButton() {
+  const { user } = await auth()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center gap-3 outline-none">
+        <div className="flex flex-col items-end">
+          <span className="text-sm font-medium">{user.name}</span>
+          <span className="text-muted-foreground text-xs">{user.email}</span>
+        </div>
+        <Avatar>
+          {user.avatarUrl && (
+            <AvatarImage src="https://github.com/devjpedro.png" />
+          )}
+          <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
+        </Avatar>
+        <ChevronDown className="text-muted-foreground size-4" />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem asChild>
+          <a href="/api/auth/sign-out">
+            Sign Out
+            <LogOut className="mr-2 size-4" />
+          </a>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
