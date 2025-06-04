0d9ed92f1b1b6e612f45347f4201611f401cdab5
buckz <joaopedross.ti9@gmail.com>
2025-06-04 20:39:21 -0300

    add: environment variables

--- apps/web/src/components/theme/theme-switcher.tsx
+++ apps/web/src/components/theme/theme-switcher.tsx
@@ -13,17 +13,14 @@ import {
 } from '../ui/dropdown-menu'
 
 export default function ThemeSwitcher() {
-  const { resolvedTheme, setTheme } = useTheme()
+  const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          {resolvedTheme === 'light' ? (
-            <Sun className="size-4" />
-          ) : (
-            <Moon className="size-4" />
-          )}
+          <Sun className="size-4 dark:hidden" />
+          <Moon className="hidden size-4 dark:block" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
